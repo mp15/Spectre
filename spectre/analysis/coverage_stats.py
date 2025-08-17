3f2142129ce5868b3bd2f152ebfdf704ae94c5ea
Martin Pollard <mp15@sanger.ac.uk>
2025-08-17 01:12:41 +0100

    np.NaN to np.nan

--- spectre/analysis/coverage_stats.py
+++ spectre/analysis/coverage_stats.py
@@ -4,18 +4,18 @@ import numpy as np
 class CoverageStatistics(object):
     # should have "get" and "set" func but just use them directly
     def __init__(self):
-        # {"average": np.NaN, "std_dev": np.NaN, "min": np.NaN, "max": np.NaN}
+        # {"average": np.nan, "std_dev": np.nan, "min": np.nan, "max": np.nan}
         # Stats
         self.chromosome_len = 0
         self.chromosome_name = ""
-        self.average = np.NaN
-        self.std_dev = np.NaN
-        self.min = np.NaN
-        self.max = np.NaN
-        self.median = np.NaN
-        self.genome_wide_median = np.NaN
-        self.genome_wide_mean = np.NaN
-        self.coverage_overwrite = np.NaN
+        self.average = np.nan
+        self.std_dev = np.nan
+        self.min = np.nan
+        self.max = np.nan
+        self.median = np.nan
+        self.genome_wide_median = np.nan
+        self.genome_wide_mean = np.nan
+        self.coverage_overwrite = np.nan
         self.empty = True
 
     def print(self):
@@ -34,9 +34,9 @@ class CoverageData(object):
     # should have "get" and "set" func but just use them directly
     def __init__(self):
         # raw data
-        self.coverage_raw = np.NaN
-        self.positions = np.NaN
-        self.coverage_log2 = np.NaN         # log2(x)
-        self.normalized_cov = np.NaN        # x/median
-        self.normalized_cov_ploidy = np.NaN   # x/median * 2 -> for diploid
+        self.coverage_raw = np.nan
+        self.positions = np.nan
+        self.coverage_log2 = np.nan         # log2(x)
+        self.normalized_cov = np.nan        # x/median
+        self.normalized_cov_ploidy = np.nan   # x/median * 2 -> for diploid
         self.empty = True
